c1f82283729d6be310b9bd94c52f39b2f4b49c1a
maquino <maquino@codigobyte.com.ar>
2021-08-27 13:54:01 -0300

    Seguimos con pruebas url no object

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -50,11 +50,9 @@
                 
                 <article class="w-full mb-12 h-80 bg-cover bg-center @if($loop->first) md:col-span-2 @endif" 
                     style="
-                        @if($post->images) 
-                            background-image:url({{ Storage::url($post->images->first()->url) }}) 
-                        @else 
+                        
                             background-image:url({{asset('img/fondo/fondoPost.webp')}}) 
-                        @endif
+                        
                         ">
                     
                     <div class="w-full h-full px-8 flex flex-col justify-center">
